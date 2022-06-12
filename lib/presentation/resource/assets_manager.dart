a62d5363807fe3f9e0ea64a43eab53d4dfca7d43
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-06-12 20:25:13 +0200

    delete img and add svg with assets manager

---
+++ lib/presentation/resource/assets_manager.dart
@@ -0,0 +1,6 @@
+class AssetsManager{
+  static const String onBoardingImg = "assets/svg/illustration.svg";
+  static const String onBoardingImg2 = "assets/svg/illustration-1.svg";
+  static const String onBoardingImg3 = "assets/svg/illustration-2.svg";
+  static const String welcomeIntro = "assets/svg/Icon.svg";
+}

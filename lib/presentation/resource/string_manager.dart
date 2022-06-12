de85002ce88d3e474f31dab9526bd58cfcf91b38
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-06-12 19:44:01 +0200

    add assets img

--- lib/presentation/resource/string_manager.dart
+++ lib/presentation/resource/string_manager.dart
@@ -1,4 +1,39 @@
 class AppString {
   static String secondaryColor = '';
-
 }
+
+class SplashScreen {
+  static String title = 'E-Courses';
+  static String subtitle = 'e-Learning Course App';
+}
+
+class FirstOnBoardingScreen {
+  static String title = '''
+  E-courses free 
+  trial courses
+  ''';
+  static String subtitle = '''
+  Free courses for you to find your 
+  way to learning ''';
+}
+
+class SecondOnBoardingScreen {
+  static String title = '''
+  Quick and easy learning
+  ''';
+  static String subtitle = '''
+  Easy and fast learning at any time to help you improve 
+  various skills
+  ''';
+}
+
+class ThirdOnBoardingScreen {
+  static String title = '''
+  Create your own study plan
+  ''';
+  static String subtitle = '''
+  Study according to the study plan, make study more 
+  motivated
+  ''';
+}
+  
